77baf6bbb4beab5d5c31f0d365d2b7cd19a43f4d
Gabriele Lorenzo <gabriele.lorenzo6@gmail.com>
2025-03-12 16:28:27 +0100

    Base files added

---
+++ src/finetune.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--hf_token", type=str, required=False)
+args = parser.parse_args()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
